76cb0108f19098d1796b59546937b15d85a7feeb
petersenjoern <petersenjoern@protonmail.com>
2022-03-25 07:57:55 +0100

    restructuring code locations

--- src/baseline.py
+++ src/baseline.py
@@ -2,10 +2,9 @@
 # Libraries
 import pathlib
 import yaml
-from utils.misc import seed_env, load_and_prepare_nbme_data
+from utils.misc import seed_env, load_and_prepare_nbme_data, format_annos_from_same_patient
 import tensorflow as tf
-import pandas as pd
-from typing import Tuple, List
+
 
 # Paths
 PATH_BASE = pathlib.Path(__file__).absolute().parents[1]
@@ -13,7 +12,6 @@ PATH_YAML = PATH_BASE.joinpath("config.yaml")
 
 # Constants
 EPSILON = tf.keras.backend.epsilon() #to prevent divide by zero error
-
 
 # Setup
 with open(PATH_YAML, "r") as file:
@@ -26,70 +24,20 @@ seed_env(424)
 cfg.get("datasets")
 df=load_and_prepare_nbme_data(paths=cfg["datasets"], train=True)
 
-
-# %%
-df.head()
+# reformat texts and its annotations per patient
+# to [{
+#   "content": n_history, 
+#   "annotations": [{
+#       "start": location[0],
+#       "stop": location[1],
+#       "text": annotation,
+#       "tag": feature_text
+#   },{...}]
+# },{...}]
 
 
 #%%
-# convert annotation, location, feature_text, pn_history
-# to [{"content": n_history, "annotations": [{"start": location[0], "stop": location[1], "text": annotation, "tag": feature_text}]}]
-
-def yield_rows_with_same_patient_id(df: pd.DataFrame):
-    """
-    Yield all records that have the same patient history so,
-    they can be processed together subsequently
-    """
-
-    pn_nums = set()
-    for _,row in df.iterrows():
-        pn_num = int(row["pn_num"])
-        if pn_num in pn_nums:
-            continue
-
-        pn_nums.add(pn_num)
-        # for each patient return all valid (not empty []) rows with annotations
-        mask = (df["pn_num"] == pn_num) & (df["annotation"] != "[]")
-        df_for_one_patient = df.loc[mask]
-        yield df_for_one_patient
-
-
-def format_anno_from_same_patient(df: pd.DataFrame) -> Tuple[List, List]:
-    """
-    For each patient dataset (each contains 16 rows), prepare the data
-    to a {"content": "patient has visited the doctor on the xx", 
-    "annotations": [{"start": 1, "end": 2, "text": "at", "tag": "something"}]}
-    """
-
-    preprocessed_data = []
-    unique_labels = set()
-    for patient_subset_df in yield_rows_with_same_patient_id(df):
-        annotations = []
-        for _,row in patient_subset_df.iterrows():
-            tag = str(row["feature_text"])
-            unique_labels.add(tag)
-            content = str(row["pn_history"])
-            
-            # clean annotation locations like:
-            # "['696 724', '123 456']" and "['intermittent episodes', 'episode']"
-            annos = row["annotation"].replace("'","").strip("][").split(', ')
-            anno_locs = row["location"].strip("']['").split(', ')
-            # for each list item, try to split other signs like ;
-            anno_locs = [anno_loc.replace("'", "").split(";") for anno_loc in anno_locs]
-            
-            #flatten list of lists to list
-            anno_locs = [item for sublist in anno_locs for item in sublist]
-            for anno_loc, _anno in zip(anno_locs, annos):
-                start, end = tuple(anno_loc.replace("'", "").split(" "))
-                start, end = int(start), int(end)
-                text = content[start:end]
-                annotations.append(dict(start=start, end=end, text=text, tag=tag))
-            preprocessed_example = {"content": content, "annotations": annotations}
-            preprocessed_data.append(preprocessed_example)
-    return preprocessed_data, unique_labels
-
-#%%
-data, unique_labels = format_anno_from_same_patient(df)
+data, unique_labels = format_annos_from_same_patient(df)
 print(data, unique_labels)
 # %%
 # from tf.keras.utils import Sequence
--- src/utils/misc/__init__.py
+++ src/utils/misc/__init__.py
@@ -4,7 +4,7 @@ import random
 import numpy as np
 import tensorflow as tf
 import pandas as pd
-from typing import Dict
+from typing import Dict, Tuple, List
 
 def seed_env(seed: int) -> None:
     """seed various services & libraries"""
@@ -62,3 +62,57 @@ def manual_curation_of_entries(df: pd.DataFrame) -> pd.DataFrame:
     
     df.loc[(df["location"] == "['85 99', '126 138', '126 131;143 151']"), "location"] = "['85 99', '126 151']"
     return df
+
+
+def yield_rows_with_same_patient_id(df: pd.DataFrame):
+    """
+    Yield all records that have the same patient history so,
+    they can be processed together subsequently
+    """
+
+    pn_nums = set()
+    for _,row in df.iterrows():
+        pn_num = int(row["pn_num"])
+        if pn_num in pn_nums:
+            continue
+
+        pn_nums.add(pn_num)
+        # for each patient return all valid (not empty []) rows with annotations
+        mask = (df["pn_num"] == pn_num) & (df["annotation"] != "[]")
+        df_for_one_patient = df.loc[mask]
+        yield df_for_one_patient
+
+
+def format_annos_from_same_patient(df: pd.DataFrame) -> Tuple[List, List]:
+    """
+    For each patient dataset (each contains 16 rows), prepare the data
+    to a {"content": "patient has visited the doctor on the xx", 
+    "annotations": [{"start": 1, "end": 2, "text": "at", "tag": "something"}]}
+    """
+
+    preprocessed_data = []
+    unique_labels = set()
+    for patient_subset_df in yield_rows_with_same_patient_id(df):
+        annotations = []
+        for _,row in patient_subset_df.iterrows():
+            tag = str(row["feature_text"])
+            unique_labels.add(tag)
+            content = str(row["pn_history"])
+            
+            # clean annotation locations like:
+            # "['696 724', '123 456']" and "['intermittent episodes', 'episode']"
+            annos = row["annotation"].replace("'","").strip("][").split(', ') # can be used as assert
+            anno_locs = row["location"].strip("']['").split(', ')
+            # for each list item, try to split other signs like ;
+            anno_locs = [anno_loc.replace("'", "").split(";") for anno_loc in anno_locs]
+            
+            #flatten list of lists to list
+            anno_locs = [item for sublist in anno_locs for item in sublist]
+            for anno_loc, _anno in zip(anno_locs, annos):
+                start, end = tuple(anno_loc.replace("'", "").split(" "))
+                start, end = int(start), int(end)
+                text = content[start:end]
+                annotations.append(dict(start=start, end=end, text=text, tag=tag))
+            preprocessed_example = {"content": content, "annotations": annotations}
+            preprocessed_data.append(preprocessed_example)
+    return preprocessed_data, unique_labels
