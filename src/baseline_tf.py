cc2ee0401ae6f5e3f10058ec9a11bc4f8d704022
petersenjoern <petersenjoern@protonmail.com>
2022-06-25 12:58:01 +0200

    Add datasets to tf

--- src/baseline_tf.py
+++ src/baseline_tf.py
@@ -9,9 +9,14 @@ import pandas as pd
 import tensorflow as tf
 print('TF version',tf.__version__)
 
-from tqdm.auto import tqdm
-from typing import List
+import datasets
+from datasets import load_dataset, Dataset
+print(f"datasets version: {datasets.__version__}")
 
+from tqdm.auto import tqdm
+from typing import List, Dict
+from sklearn.metrics import f1_score
+from sklearn.model_selection import StratifiedKFold, GroupKFold, KFold
 
 #%%
 ## Directories/ Paths
@@ -22,26 +27,30 @@ OUTPUT_DIR_TOKENIZER=OUTPUT_DIR.joinpath('tokenizer')
 
 
 #%%
-## Custom imports
+## Custom imports mostly due to root permissions in container
 os.system('pip uninstall -y transformers')
-# pip download transformers && pip download tokenizers
-os.system(f'python -m pip install --no-index --find-links={str(BASE_DIR.joinpath("data","input","nbme-pip-wheels-transformers"))} transformers')
 os.system(f'python -m pip install --no-index --find-links={str(BASE_DIR.joinpath("data","input","nbme-pip-wheels-tokenizers"))} tokenizers')
 import tokenizers
-import transformers
 print(f"tokenizers.__version__: {tokenizers.__version__}")
-print(f"transformers.__version__: {transformers.__version__}")
-from transformers import AutoTokenizer, AutoModel, AutoConfig
 
+os.system(f'python -m pip install --no-index --find-links={str(BASE_DIR.joinpath("data","input","nbme-pip-wheels-transformers"))} transformers')
+import transformers
+print(f"transformers.__version__: {transformers.__version__}")
+from transformers import AutoTokenizer, AutoModel, AutoConfig, DataCollatorWithPadding
 
 #%%
 ## Configuration
 class CFG:
     debug=True
+    fold_n=5
     modelName="microsoft/deberta-base"
     epochs=5
     seed=42
     train=True
+    tokenizer=None
+    max_len=None
+    batch_size=2
+
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
@@ -55,6 +64,7 @@ def seed_everything(seed:int=42) -> None:
     os.environ['PYTHONHASHSEED'] = str(seed)
     np.random.seed(seed)
     tf.random.set_seed(seed)
+    os.environ["TF_CUDNN_DETERMINISTIC"] = str(seed)
 
 def load_train_or_val_data(path_to_file: pathlib.Path) -> pd.DataFrame:
     """Load dataset for training or validation"""
@@ -111,6 +121,54 @@ def get_max_len_for_feature(df: pd.DataFrame, feature_names: List[str], tokenize
         print(f'{text_col} max(lengths): {max(feature_lengths)}')
     return max(feature_lengths)
 
+# TODO: example type annotation (dataset one record)
+def prepare_input(example) -> Dict[str, int]:
+    """Tokenize input and return the tokenization result.
+    Returns: 'attention_mask', 'input_ids', 'token_type_ids'
+    """
+
+    inputs = CFG.tokenizer(example["pn_history"], example["feature_text"], 
+                           add_special_tokens=True,
+                           max_length=CFG.max_len,
+                           padding="max_length",
+                           return_offsets_mapping=False)
+    return inputs["input_ids"], inputs["attention_mask"], inputs["token_type_ids"]
+
+
+# TODO: step through and annotate and comment
+def prepare_label(example) -> Dict[str, int]:
+    encoded = CFG.tokenizer(example["pn_history"],
+                            add_special_tokens=True,
+                            max_length=CFG.max_len,
+                            padding="max_length",
+                            return_offsets_mapping=True)
+    offset_mapping = encoded['offset_mapping']
+    ignore_idxes = np.where(np.array(encoded.sequence_ids()) != 0)[0]
+    label = np.zeros(len(offset_mapping))
+    label[ignore_idxes] = -1
+    if example["annotation_length"] != 0:
+        for location in example["location"]:
+            for loc in [s.split() for s in location.split(';')]:
+                start_idx = -1
+                end_idx = -1
+                start, end = int(loc[0]), int(loc[1])
+                for idx in range(len(offset_mapping)):
+                    if (start_idx == -1) & (start < offset_mapping[idx][0]):
+                        start_idx = idx - 1
+                    if (end_idx == -1) & (end <= offset_mapping[idx][1]):
+                        end_idx = idx + 1
+                if start_idx == -1:
+                    start_idx = end_idx
+                if (start_idx != -1) & (end_idx != -1):
+                    label[start_idx:end_idx] = 1
+    return label
+
+def preprocess_input_and_label(example):
+    """Preprocess input and labels at the same time"""
+    example["input_ids"], example["attention_mask"], example["token_type_ids"] = prepare_input(example)
+    example["labels"] = prepare_label(example)
+    return example
+
 #%%
 ## Execution (main function)
 
@@ -124,15 +182,68 @@ if __name__ == "__main__":
 
     df_train = df_train.merge(df_features, on=['feature_num', 'case_num'], how='left')
     df_train = df_train.merge(df_patient_notes, on=['pn_num', 'case_num'], how='left')
+
+
+    # Add KFold label to data
+    #TODO: extract this as a function
+    Fold = GroupKFold(n_splits=CFG.fold_n)
+    groups = df_train['pn_num'].values
+    for n, (_, val_index) in enumerate(Fold.split(df_train, df_train['location'], groups)):
+        df_train.loc[val_index, 'fold'] = int(n)
+    df_train['fold'] = df_train['fold'].astype(int)
+    print(df_train.groupby('fold').size())
     
     # Tokenizer, Model
-    tokenizer = get_tokenizer(CFG.modelName)
-    max_len_pn_history = get_max_len_for_feature(df_train, ["pn_history"], tokenizer)
-    max_len_feature_text = get_max_len_for_feature(df_train, ["feature_text"], tokenizer)
-    max_len = max_len_pn_history + max_len_feature_text + len(["CLS", "SEP", "SEP"])
+    CFG.tokenizer = get_tokenizer(CFG.modelName)
+    max_len_pn_history = get_max_len_for_feature(df_train, ["pn_history"], CFG.tokenizer)
+    max_len_feature_text = get_max_len_for_feature(df_train, ["feature_text"], CFG.tokenizer)
+    CFG.max_len = max_len_pn_history + max_len_feature_text + len(["CLS", "SEP", "SEP"])
+
+    # Prepare train and test data
+    dataset = Dataset.from_pandas(df_train)
+    dataset_splitted=dataset.train_test_split(test_size=0.1)
+
+
+    # TODO: preprocess test data as well dataset_splitted["test"]
+    dataset_tf_test = dataset_splitted_xxxxx["test"].to_tf_dataset(
+        columns=["input_ids", "token_type_ids", "attention_mask"],
+        label_cols=["labels"],
+        batch_size=CFG.batch_size,
+        collate_fn=DataCollatorWithPadding(tokenizer=CFG.tokenizer, return_tensors="tf"),
+        shuffle=True
+    )
+
+
+
+    # Prepare data for each fold
+    REMOVE_COLS = ['id', 'case_num', 'pn_num', 'feature_num', 'annotation', 'location', 'annotation_length', 'feature_text', 'pn_history', '__index_level_0__']
+    dataset_transformed = dataset["train"].map(preprocess_input_and_label, num_proc=os.cpu_count()-2, remove_columns=REMOVE_COLS)
+    
+    iteration_n = 0
+    dataset_transformed_train_fold = dataset_transformed.filter(lambda example: example['fold'] != iteration_n)
+    dataset_transformed_val_fold = dataset_transformed.filter(lambda example: example['fold'] == iteration_n)
+
+    dataset_tf_train = dataset_transformed_train_fold.to_tf_dataset(
+        columns=["input_ids", "token_type_ids", "attention_mask"],
+        label_cols=["labels"],
+        batch_size=CFG.batch_size,
+        collate_fn=DataCollatorWithPadding(tokenizer=CFG.tokenizer, return_tensors="tf"),
+        shuffle=True
+    )
+
+    dataset_tf_val = dataset_transformed_val_fold.to_tf_dataset(
+        columns=["input_ids", "token_type_ids", "attention_mask"],
+        label_cols=["labels"],
+        batch_size=CFG.batch_size,
+        collate_fn=DataCollatorWithPadding(tokenizer=CFG.tokenizer, return_tensors="tf"),
+        shuffle=True
+    )
 
 
 #%%
 
+
+
+#%%
 
 # %%
--- src/utils/misc/misc_tf.py
+++ src/utils/misc/misc_tf.py
@@ -5,6 +5,9 @@ import tensorflow as tf
 #TODO: add logging
 def define_gpu_strategy():
     """Define the GPU strategy for modelling."""
+
+    gpu_available = tf.config.list_physical_devices('GPU')
+
     if os.environ["CUDA_VISIBLE_DEVICES"].count(',') == 0:
         strategy = tf.distribute.get_strategy()
     else:
